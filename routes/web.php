c7547d7b99aef607caabb075a295c9e67297a864
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-26 16:06:52 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,37 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('users', function (){
+    return 'Users';
+});
+
+// ルーティングがかぶる場合の解決法１idは数字だけを受け付けるようにする
+//Route::get('users/{id}', function ($id){
+//   return "Details for user: {$id}";
+//})->where('id', '[0-9]+');
+
+// ルーティングがかぶる場合の解決法２順番を変える
+Route::get('users/new', function (){
+   return 'Create new user';
+});
+
+Route::get('users/{id}', function ($id){
+   return "Details for user: {$id}";
+});
+
+// 複数引数
+//Route::get('hello/{name}/{nickname}', function ($name, $nickname){
+//   return "Hi, {$name}. your nickname is: {$nickname}";
+//});
+
+// 複数引数条件付き
+Route::get('hello/{name}/{nickname?}', function ($name, $nickname = null){
+    if ($nickname) {
+        return "Hi, {$name}. your nickname is: {$nickname}";
+    } else {
+        return "Hi, {$name}. nickname is not defined!";
+    }
 });
